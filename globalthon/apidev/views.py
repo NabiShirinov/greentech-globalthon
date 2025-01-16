71c8bf4a8ee9c33dd7c679df12842c9fcc19bed8
NabiShirinov <117586522+NabiShirinov@users.noreply.github.com>
2025-01-17 00:17:11 +0900

    test

---
+++ globalthon/apidev/views.py
@@ -0,0 +1,12 @@
+from django.http import JsonResponse
+from apidev.serializers import ProductSerializer
+from apidev.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+# Create your views here.
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
